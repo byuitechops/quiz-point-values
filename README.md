491e780892042aaacdf0243309605095f1c57cf5
YuriiVasiuk <yuravasiuk@gmail.com>
2018-04-09 17:42:35 -0600

    The module is ready for further development.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# quiz-point-values
+# Quiz Point Values
+### *Package Name*: quiz-point-values
+### *Child Type*: ???
+### *Platform*: online
+### *Required*: ???
+
+This child module is built to be used by the Brigham Young University - Idaho D2L to Canvas Conversion Tool. It utilizes the standard `module.exports => (course, stepCallback)` signature and uses the Conversion Tool's standard logging functions. You can view extended documentation [Here](https://github.com/byuitechops/d2l-to-canvas-conversion-tool/tree/master/documentation).
+
+## Purpose
 The module identifies quizzes where total point values change.
+
+## How to Install
+npm install quiz-point-values
+
+## Run Requirements
+Does not apply
+
+## Options
+Does not apply
+
+## Outputs
+???
+
+## Process
+???
+
+## Log Categories
+???
+
+## Requirements
+???
--- main.js
+++ main.js
@@ -7,19 +7,10 @@
 /* Include this line only if you are going to use Canvas API */
 // const canvas = require('canvas-wrapper');
 
-/* View available course object functions */
-// https://github.com/byuitechops/d2l-to-canvas-conversion-tool/blob/master/documentation/classFunctions.md
 
 module.exports = (course, stepCallback) => {
-    /* Create the module report so that we can access it later as needed.
-    This MUST be done at the beginning of each child module. */
-    course.addModuleReport('moduleName');
 
-    /* Used to log successful actions */
-    course.success('moduleName', 'moduleName successfully ...');
-
-    /* How to report an error (Replace "moduleName") */
-    // course.throwErr('moduleName', e);
+    console.log('Desmond the moon bear!');
 
     /* You should never call the stepCallback with an error. We want the
     whole program to run when testing so we can catch all existing errors */
